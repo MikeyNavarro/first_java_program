f57f4be597062fe4b814bf1483f6205a6c801468
Mikey Navarro <mikey.navarro.135@gmail.com>
2022-07-31 16:56:22 -0700

    first commit

---
+++ Info.java
@@ -0,0 +1,7 @@
+public class Info{
+  public static void main(String[] args){
+    System.out.println("my name is Mikey Navarro,");
+    System.out.println("I am 22 years old");
+    System.out.println("My home Town is Fairfield CA");
+  }
+}
